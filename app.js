e6aeaf61dbb8ace139e845a213b0149fa899869d
Karim Mohamed <komicho1996@gmail.com>
2018-12-06 10:43:35 +0200

    create test xss

--- app.js
+++ app.js
@@ -7,6 +7,6 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/xss', (req, res) => res.send('Hello xss => ' + req.query.xss))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
